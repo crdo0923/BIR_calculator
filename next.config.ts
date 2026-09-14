51507b5d972d6128798abb9ab773139856eaf1c4
Airship Agent <airship-agent@example.com>
2026-09-14 14:38:39 +0800

    chore: disable devIndicators in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.100.143", "192.168.100.*", "localhost"],
+  devIndicators: false,
 };
 export default nextConfig;
